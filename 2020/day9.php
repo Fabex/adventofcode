a2d241d5f126f6c72f076e8eb591798b39b0c826
Fabien Lalanne <fabien.lalanne@bedrockstreaming.com>
2020-12-09 15:47:55 +0100

    day9 cs

--- 2020/day9.php
+++ 2020/day9.php
@@ -20,32 +20,32 @@ $the25 = array_slice($data, 0, 25);
 $others = array_slice($data, 25);
 $sums = calcSum($the25);
 
-$invalidNumber = 0;
+$step1 = 0;
 for ($i = 25, $iMax = count($data); $i <= $iMax; $i++) {
-    $number = (int) $data[$i];
+    $number = (int)$data[$i];
     if (false === in_array($number, $sums)) {
-        $invalidNumber = $number;
+        $step1 = $number;
         break;
     }
-    $the25 = array_slice($data, $i-25+1, 25);
-    $others = array_slice($data, 25+$i);
+    $the25 = array_slice($data, $i - 25 + 1, 25);
+    $others = array_slice($data, 25 + $i);
     $sums = calcSum($the25);
 }
 
-$data = array_filter($data, function($item) use ($invalidNumber) {
-    return $item < $invalidNumber;
+$data = array_filter($data, function ($item) use ($step1) {
+    return $item < $step1;
 });
 
 $step2 = [];
 for ($start = 0, $startMax = count($data); $start < $startMax; $start++) {
     for ($end = 1, $endMax = count($data); $end < $endMax; $end++) {
         $sum = array_sum($t = array_slice($data, $start, $end));
-        if($sum === $invalidNumber) {
+        if ($sum === $step1) {
             $step2 = $t;
             break 2;
         }
     }
 }
 
-dump($invalidNumber);
-dump(min($step2)+max($step2));
+dump($step1);
+dump(min($step2) + max($step2));
