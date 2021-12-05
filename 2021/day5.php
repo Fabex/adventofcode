3aac8e7baa0f883c16e57d947a560bd258a9be7b
Fabien Lalanne <fabien.lalanne@bedrockstreaming.com>
2021-12-05 21:12:16 +0100

    add display for 2021 day 5

--- 2021/day5.php
+++ 2021/day5.php
@@ -32,7 +32,7 @@ function drawDiag(&$matrix, $startX, $startY, $endX, $endY)
 {
     if ($startX < $endX) {
         for (; $startX <= $endX; $startX++) {
-            $idx = $startX . '-' . ($startY < $endY ? $startY++: $startY--);
+            $idx = $startX . '-' . ($startY < $endY ? $startY++ : $startY--);
             $matrix[$idx] = isset($matrix[$idx]) ? $matrix[$idx] + 1 : 1;
         }
         return;
@@ -40,7 +40,7 @@ function drawDiag(&$matrix, $startX, $startY, $endX, $endY)
 
     if ($startX > $endX) {
         for (; $startX >= $endX; $startX--) {
-            $idx = $startX . '-' . ($startY < $endY ? $startY++: $startY--);
+            $idx = $startX . '-' . ($startY < $endY ? $startY++ : $startY--);
             $matrix[$idx] = isset($matrix[$idx]) ? $matrix[$idx] + 1 : 1;
         }
         return;
@@ -61,5 +61,32 @@ foreach ($instructions as $instruction) {
     //step1 remove line below
     drawDiag($matrix, (int)$startX, (int)$startY, (int)$endX, (int)$endY);
 }
+$t = [];
+foreach ($matrix as $coord => $value) {
+    [$x, $y] = explode('-', $coord);
+    $t[$x][$y] = $value;
+}
 
+foreach ($t as $i => $v) {
+    $t[$i] = $v + array_fill_keys(range(min(array_keys($v)), max(array_keys($v))), ' ');
+    ksort($t[$i]);
+}
+
+$u = $t + array_fill_keys(range(min(array_keys($t)), max(array_keys($t))), '');
+
+ksort($u);
+foreach ($u as $v) {
+    foreach ($v as $i) {
+        echo match ((string)$i) {
+            ' ' => " ",
+            '1' => "\e[96m*\e[0m",
+            '2' => "\e[94m*\e[0m",
+            '3' => "\e[92m*\e[0m",
+            '4' => "\e[93m*\e[0m",
+            '5' => "\e[91m*\e[0m",
+        };
+    }
+    echo "\n";
+}
 dump(count(array_filter($matrix, static fn($item) => $item >= 2)));
+
